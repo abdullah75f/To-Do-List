c70b440cd254d8de002374229366414045804a4d
zaz7 <abdullah75farid@gmail.com>
2024-07-14 16:17:34 +0300

    efefefrfrf

--- project-4.js
+++ project-4.js
@@ -3,3 +3,8 @@
 const addTask = document.getElementById("add-task");
 const taskContainer = document.getElementById("task-container");
 const inputTask = document.getElementById("input-task");
+
+
+//Event Listener for Add Button
+
+addTask.addEventListener("click", function())
